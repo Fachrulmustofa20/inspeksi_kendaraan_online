6d4c8c6e1b596cb889ebb2050701d33b094e56fd
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2020-02-15 21:37:51 +0700

    fix login

--- login.php
+++ login.php
@@ -40,7 +40,7 @@
         <!-- ============================================================== -->
         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative" style="background:url(assets/images/big/auth-bg.jpg) no-repeat center center;">
             <div class="auth-box row">
-                <div class="col-lg-7 col-md-5 modal-bg-img" style="background-image: url(assets/images/big/10.jpg);">
+                <div class="col-lg-7 col-md-5 modal-bg-img" style="background-image: url(assets/images/big/9.jpg);">
                 </div>
                 <div class="col-lg-5 col-md-7 bg-white">
                     <div class="p-3">
